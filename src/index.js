6ea9f02a096467c2eb762c68cc9496a0a0b6dedd
Ravindra Chandanshiv <rc16988@gmail.com>
2021-02-28 13:30:57 +0530

    Create gh-pages branch with static content

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Ravi from './components/login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Ravi />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
